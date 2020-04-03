44fd9c4e69c16807e50133e49132f93e47c1e618
Everton Roberto <everton_roliveira@outlook.com>
2020-04-03 15:33:26 -0300

    feat: ensure SignUpController returns 400 if an invalid email is provided

--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -1,7 +1,6 @@
-import { MissingParamError } from '../errors/missing-param-error'
 import { HttpResponse } from '../protocols/http'
 
-export const badRequest = (error: MissingParamError): HttpResponse => ({
+export const badRequest = (error: Error): HttpResponse => ({
   statusCode: 400,
   body: error
 })
