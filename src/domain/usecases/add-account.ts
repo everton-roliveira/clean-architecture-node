d8bbfb9f2c4f7a3c8f47e098f780d86dca071512
Everton Roberto <everton_roliveira@outlook.com>
2020-04-22 20:54:07 -0300

    feat: ensure SignUpController calls AddAccount with correct values

---
+++ src/domain/usecases/add-account.ts
@@ -0,0 +1,11 @@
+import { AcountModel } from '../models/account'
+
+export interface AddAccountModel {
+  name: string
+  email: string
+  password: string
+}
+
+export interface AddAccount {
+  add (account: AddAccountModel): AcountModel
+}
