f9616219f6168ba5488dce0fd85970a22817deca
Everton Roberto <everton_roliveira@outlook.com>
2020-05-21 21:46:06 -0300

    feat: ensure cors is enabled

---
+++ src/main/middlewares/cors.test.ts
@@ -0,0 +1,17 @@
+import request from 'supertest'
+import app from '../config/app'
+
+describe('CORS Middleware', () => {
+  test('should enable CORS', async () => {
+    const URL = '/test_cors'
+
+    app.get(URL, (req, res) => {
+      res.send()
+    })
+    await request(app)
+      .get(URL)
+      .expect('access-control-allow-origin', '*')
+      .expect('access-control-allow-methods', '*')
+      .expect('access-control-allow-headers', '*')
+  })
+})
